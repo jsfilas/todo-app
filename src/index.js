d542280be200be2985d9a35c88afc6a1ff64b208
Jackson Filas <jsfilas@gmail.com>
2020-11-11 11:25:56 -0700

    designed HTML and CSS to be visually appealing and get general layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("hello");
+
+/* function newProject(name) {
+    let projectsContainer = document.querySelector("#projects");
+    let div = document.createElement('div');
+    div.classList.add("projectFormatter");
+
+    div.innerHTML = name;
+
+    projectsContainer.appendChild(div);
+
+} */
