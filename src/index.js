c4cd0c306e391d52e697d510cd85c2904fca1f4f
Jackson Filas <jsfilas@gmail.com>
2020-11-17 17:02:39 -0700

    created basic functions to create a project, store it in an array, and delete it from the UI and the array at the same time. Project class used and has todo array for each to store individual todos.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,115 @@
 console.log("hello");
 
-/* function newProject(name) {
-    let projectsContainer = document.querySelector("#projects");
-    let div = document.createElement('div');
-    div.classList.add("projectFormatter");
+let myProjects = [];
 
-    div.innerHTML = name;
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+}
 
-    projectsContainer.appendChild(div);
 
-} */
+
+let todo = (function(){
+    //dom
+    let newProjectBtn = document.querySelector('#newProject');
+    let addProjectBtn = document.querySelector('#projectSubmit');
+    let cancelProjectBtn = document.querySelector('#projectCancel');
+    let projectCreator = document.querySelector('#PI');
+    let projectsContainer = document.querySelector("#projectsContainer");
+    
+  
+    
+    newProjectBtn.addEventListener('click', showProjectCreator);
+    addProjectBtn.addEventListener('click', newProject);
+    cancelProjectBtn.addEventListener('click', cancelProject);
+
+
+    
+  
+    function showProjectCreator() {
+      projectCreator.classList.remove('hidden');
+    }
+
+    function newProject() {
+        const name = document.querySelector('#projectInput').value;
+        myProjects.push(new Project(name));
+        console.log(myProjects);
+        cancelProject();
+        resetProjectView();
+        displayProjects();
+    }
+
+    function displayProjects() {
+
+        myProjects.forEach(element => {
+            let div = document.createElement('div');
+            let btn = document.createElement('button');
+            btn.classList.add("deleteProjectButton");
+            div.classList.add("projectFormatter");
+            let name = element.name
+            btn.innerHTML = 'DELETE';
+            div.innerHTML = name;
+            
+            projectsContainer.appendChild(div);
+            div.appendChild(btn);
+            btn.addEventListener('click', deleteProject);
+            console.log(myProjects);
+        });
+        
+    } 
+
+   
+    function resetProjectView() {
+        projectsContainer.innerHTML = '';
+    
+    }
+    function deleteProject(e) {
+        let deleteBtn = $(e.target).closest('button');
+        let closestDiv = deleteBtn.closest('div');
+        let project = closestDiv.html();
+        console.log(closestDiv);
+        console.log(project);
+
+        for(var i =0; i < myProjects.length; i++) {
+            if (project.includes(myProjects[i].name)) {
+                myProjects.splice(i, 1);
+                console.log(myProjects);
+                deleteBtn = '';
+                closestElement = '';
+                project = '';
+                
+            }
+        }
+        resetProjectView();
+        displayProjects();
+    }
+
+    /*function deleteProject() {
+        let deleteBtn = document.querySelector('.clicked');
+        let closestElement = deleteBtn.parentElement;
+        let project = closestElement.textContent;
+        console.log(project);
+
+        for(var i =0; i < myProjects.length; i++) {
+            if (project.includes(myProjects[i].name)) {
+                myProjects.splice(i, 1);
+                console.log(myProjects);
+                deleteBtn = '';
+                closestElement = '';
+                project = '';
+                
+            }
+        }
+        resetProjectView();
+        displayProjects();
+    } */
+  
+    function cancelProject() {
+      let projectInput = document.querySelector('#projectInput').value = '';
+      projectCreator.classList.add('hidden');
+  
+    }
+  
+  })();
